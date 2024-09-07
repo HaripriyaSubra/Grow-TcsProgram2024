723dbe8d9cd791ff98530a96ee449df8fbadca07
HaripriyaSubra <57601697+HaripriyaSubra@users.noreply.github.com>
2024-09-07 18:00:28 +0200

    python data structures introduction

---
+++ 10.09.2024/python_data_structures.py
@@ -0,0 +1,19 @@
+# lists
+natural_numbers=[1,2,2,3,5]
+
+print(natural_numbers)
+
+fruits = ['apple','mango','orange']
+
+print(fruits)
+
+# dictionary
+
+person={'name':'Alice','Age':34}
+
+#tuples
+
+point=(10,20)
+
+#sets 
+even_numbers={2,4,6,8,10}
